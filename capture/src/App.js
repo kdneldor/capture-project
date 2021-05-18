1963d112b69170521f410fab08fe3f8583985f13
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-05-18 18:31:07 -0500

    7. Implemented React-Router and Added More Styling

--- capture/src/App.js
+++ capture/src/App.js
@@ -3,12 +3,28 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Imported Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
